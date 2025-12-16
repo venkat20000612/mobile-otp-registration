63a54bbeaff539bd39c2290b65bc285c3d1eee21
satheeshjinkala <satheeshjinkala@gmail.com>
2025-12-17 00:39:16 +0530

    sath01

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
   console.log(121)
+  console.log("AVR")
 });
 
 module.exports = router;
