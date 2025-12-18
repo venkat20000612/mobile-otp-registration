31e974aa47056947e3cf2c236b034b06afb7b2db
Annam Venkata Rao <venkataraoannam97@gmail.com>
2025-12-18 11:13:30 +0530

    Fix CORS issue

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
