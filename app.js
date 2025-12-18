700e7d94f68845979ceea06b592d8d18e81d0388
Annam Venkata Rao <venkataraoannam97@gmail.com>
2025-12-18 11:29:50 +0530

    Fix CORS issue

--- app.js
+++ app.js
@@ -1,71 +1,57 @@
-require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors')
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const registeruser = require('./routes/registeruser');
-const Sendotp = require('./routes/sendOtp');
-const Verifyotp = require('./routes/verifyOtp');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const registeruser = require("./routes/registeruser");
+const Sendotp = require("./routes/sendOtp");
+const Verifyotp = require("./routes/verifyOtp");
 const resendOtp = require("./routes/resendOtp");
 const loginRouter = require("./routes/login");
 const forgotPassword = require("./routes/forgotPassword");
 const resetPassword = require("./routes/resetPassword");
 
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 connectDB();
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+// MIDDLEWARES (ORDER IMPORTANT)
 app.use(logger("dev"));
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "https://astounding-brigadeiros-490b31.netlify.app"
-  ]
-}));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://astounding-brigadeiros-490b31.netlify.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// preflight
 app.options("*", cors());
-app.use(express.json());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/register', registeruser);
-app.use('/sendotp', Sendotp);
-app.use('/verifyotp', Verifyotp);
+// ROUTES
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/register", registeruser);
+app.use("/sendotp", Sendotp);
+app.use("/verifyotp", Verifyotp);
 app.use("/otp", resendOtp);
 app.use("/login", loginRouter);
 app.use("/forgotpassword", forgotPassword);
 app.use("/reset-password", resetPassword);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
